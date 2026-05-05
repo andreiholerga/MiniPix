9ca4edc5e4b2686ad6fd2bd928225c41096f47df
Andrei Holerga <andreiholerga@gmail.com>
2026-05-06 01:59:31 +0300

    added donation button

--- src/App.jsx
+++ src/App.jsx
@@ -167,6 +167,11 @@ export default function App() {
       <br />
 
       <Dropzone onFiles={handleFiles} />
+      <form action="https://www.paypal.com/donate" method="post" target="_top">
+<input type="hidden" name="hosted_button_id" value="PC59QRDDGWREY" />
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+</form>
 
       {isDone && <BigStat savedPercent={savedPercent} />}
 
@@ -177,6 +182,8 @@ export default function App() {
       <ExportButton processed={processed} files={files} />
 
       <FileList files={files} processed={processed} />
+      
+
 
       <a
         href="/privacy"
